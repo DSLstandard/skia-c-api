1c2845d7deaf0442a2a7261d16bc043dfd345b73
DSLstandard <qgeneral35@gmail.com>
2025-09-09 19:27:00 +0800

    flake: prune redundant deps

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        hPkgs = pkgs.haskell.packages."ghc966";
 
         skia129 = pkgs.callPackage ./skia-129-modified.nix {};
 
@@ -16,15 +15,6 @@
           # See https://discourse.nixos.org/t/tmux-bash-prompt-breaks-inside-of-flakes/60925/4
           pkgs.bashInteractive
 
-          pkgs.pkg-config
-
-          hPkgs.cabal-install
-          hPkgs.ghc
-          hPkgs.fourmolu
-          hPkgs.haskell-language-server
-          hPkgs.hpack
-
-          # For working with cbits/ and cbits_cmake/
           pkgs.clang
           pkgs.pkg-config
           pkgs.cmake 
@@ -41,33 +31,10 @@
 
           fontconfig # Skia fontmgr ports
           expat # Required by fontconfig
-
-          # For programs under demos/ using GLFW.
-          #
-          # The 'xorg.*' libs are learned from error messages generated when demo
-          # programs failed to launch.
-          glfw
-          xorg.libX11
-          xorg.libXi
-          xorg.libXrandr
-          xorg.libXxf86vm
-          xorg.libXcursor
-          xorg.libXinerama
-
-          # For programs under demos/ using SDL2.
-          SDL2
         ];
-
-        pyDeps = [
-            pkgs.python312
-        ] ++ (with pkgs.python311Packages; [
-            pycparser
-            pytest
-            pyyaml
-        ]);
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = devDeps ++ libDeps ++ pyDeps;
+          buildInputs = devDeps ++ libDeps;
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libDeps;
         };
       });
